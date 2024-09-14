37b0736ede522dc39d09dbddb70ab0c9758a18d1
tonystark-69 <mark69.kop1@gmail.com>
2024-09-14 20:37:38 +0600

    Update config.py

--- config.py
+++ config.py
@@ -2,13 +2,13 @@
 # Channel: https://t.me/itsSmartDev
 
 # Pyrogram setup
-API_ID = "123456"  # Replace this API ID with your actual API ID
-API_HASH = "XXXXXXXXXXX"  # Replace this API HASH with your actual API HASH
+API_ID = "28555426"  # Replace this API ID with your actual API ID
+API_HASH = "e8cc1e4bc5efd44bf39e6efb1bfeed39"  # Replace this API HASH with your actual API HASH
 SESSION_STRING = "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX"  # Replace this SESSION STRING with your actual SESSION_STRING
-BOT_TOKEN = "12345678:XXXXXXXXXXXXXX"  # Replace this BOT_TOKEN
+BOT_TOKEN = "7289187317:AAGaQf5x7PZW_JrDRxgLo-pDWBH2zUmoym8"  # Replace this BOT_TOKEN
 
 # Admin IDs
-ADMIN_IDS = [12345678, 12345678]
+ADMIN_IDS = [7118995866, 12345678]
 
 # Limits
 DEFAULT_LIMIT = 10000 # Card Scrapping Limit For Everyone
