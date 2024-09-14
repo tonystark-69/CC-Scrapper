c65a5b9a845c5db933bb4307dad434622d2fdc15
tonystark-69 <mark69.kop1@gmail.com>
2024-09-14 23:30:54 +0600

    Update scrurl.py

--- scrurl.py
+++ scrurl.py
@@ -1,17 +1,19 @@
 import re
-from urllib.parse import urlparse
 
 async def scrape_url_messages(client, channel_username, limit):
     messages = []
     count = 0
-    pattern = r'([a-zA-Z0-9-]+\.[a-zA-Z]{2,}(?:\/[^\s]*)?)'
+
+    # This regex will capture both full URLs with http(s) and domain names without a protocol
+    url_pattern = r'(https?://[^\s]+|\b[\w-]+\.\w{2,}\b)'
+
     async for message in client.search_messages(channel_username):
         if count >= limit:
             break
         text = message.text if message.text else message.caption
         if text:
-            matched_urls = re.findall(pattern, text)
-            formatted_urls = [urlparse(url).geturl() for url in matched_urls]
-            messages.extend(formatted_urls)
-            count += len(formatted_urls)
+            matched_urls = re.findall(url_pattern, text)
+            if matched_urls:
+                messages.extend(matched_urls)
+                count += len(matched_urls)
     return messages
